44783588bfb5cbe11a821497b39c2d5f62a93463
manifold <kimi.ysma@gmail.com>
2018-03-08 15:07:24 +0800

    Scripts refactor done. To be tested.

--- scripts/epcc.sh
+++ scripts/epcc.sh
@@ -55,12 +55,48 @@ run() {
             ssh ${slave} "jps | grep supervisor | cut -d' ' -f1 | xargs kill -9"
         done
         sleep 1
-    elif [ $1 = "START_FLINK" ]; then
+    elif [ $1 = "START_FLINK" ]; then       # Flink
         ${FLINK_HOME}/bin/start-cluster.sh &
         sleep 5
     elif [ $1 = "STOP_FLINK"  ]; then
         ${FLINK_HOME}/bin/stop-cluster.sh &
         sleep 5
+    elif [ $1 = "START_LOAD"  ]; then       # Data load
+        cd ../data
+        start_if_needed leiningen.core.main "Load Generation" 1 \
+            ${LEIN} run -r -t ${LOAD} --configPath ../${CONF_FILE}
+        cd ../scripts
+    elif [ $1 = "STOP_LOAD"   ]; then
+        stop_if_needed leiningen.core.main "Load Generation"
+        cd ../data
+        ${LEIN} run -g --configPath ../${CONF_FILE} || true
+        cd ../scripts
+    elif [ $1 = "START_SPARK_PROCESSING" ]; then # Spark submit
+        ${SPARK_HOME}/bin/spark-submit \
+            --master spark://dell-01.epcc:7078 \
+            --class KafkaRedisAdvertisingStream \
+            ../spark-benchmarks/target/spark-benchmarks-0.1.0.jar ../${CONF_FILE} &
+        sleep 5
+    elif [ $1 = "STOP_SPARK_PROCESSING"  ]; then
+        stop_if_needed spark.benchmark.KafkaRedisAdvertisingStream "Spark Client Process"
+    elif [ $1 = "START_STORM_PROCESSING" ]; then # Storm submit
+        ${STORM_HOME}/bin/storm jar \
+            ../storm-benchmarks/target/storm-benchmarks-0.1.0.jar storm.benchmark.AdvertisingTopology test-topo -conf ../${CONF_FILE}
+        sleep 15
+    elif [ $1 = "STOP_STORM_PROCESSING"  ]; then
+        ${STORM_HOME}/bin/storm kill -w 0 test-topo || true
+        sleep 10
+    elif [ $1 = "START_FLINK_PROCESSING" ]; then # Flink submit
+        ${FLINK_HOME} run ../flink-benchmarks/target/flink-benchmarks-0.1.0.jar --confPath ../${CONF_FILE} &
+        sleep 3
+    elif [ $1 = "STOP_FLINK_PROCESSING"  ]; then
+        FLINK_ID=`"$FLINK_HOME/bin/flink" list | grep 'Flink Streaming Job' | awk '{print $4}'; true`
+        if [ "$FLINK_ID" == "" ]; then
+            echo "Could not find streaming job to kill"
+        else
+          "$FLINK_HOME/bin/flink" cancel $FLINK_ID
+            sleep 3
+        fi
     elif [ $1 = "SPARK_TEST" ]; then
         run "START_REDIS"
         run "START_KAFKA"
@@ -95,6 +131,12 @@ run() {
 }
 
 
-
-
-echo ${SLAVES}
+if [ $# -lt 1 ]; then
+    run "HELP"
+else
+  while [ $# -gt 0 ];
+  do
+    run "$1"
+    shift
+  done
+fi
